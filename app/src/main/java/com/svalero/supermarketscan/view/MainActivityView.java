1dfd3326420862d1b3699f8b4ba5b686a2098148
HIBERUS\sballestin <sballestin@hiberus.com>
2024-05-01 13:44:03 +0200

    Pantalla de login completa

--- app/src/main/java/com/svalero/supermarketscan/view/MainActivityView.java
+++ app/src/main/java/com/svalero/supermarketscan/view/MainActivityView.java
@@ -14,6 +14,19 @@ public class MainActivityView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fullScreen();
+    }
+
+    private void fullScreen() {
+        // Oculta la barra de navegación y la barra de estado con el modo inmersivo pegajoso
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        );
     }
 
     public void calculadoraCompras(View view) {
