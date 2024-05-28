2e9795caf8568401f5dd8e278ddeda9358d5e510
Santi Stars <santi_r6@hotmail.es>
2024-05-28 02:03:45 +0200

    Configurar certificado para la API en portatil personal

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,7 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 //    implementation("com.squareup.retrofit2:converter-gson:2.4.0")
-    implementation("com.squareup.okhttp3:okhttp:4.9.1")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
 
     // CameraX core library
     implementation("androidx.camera:camera-camera2:1.3.2")
--- app/src/main/java/com/svalero/supermarketscan/api/Constants.java
+++ app/src/main/java/com/svalero/supermarketscan/api/Constants.java
@@ -1,9 +1,5 @@
 package com.svalero.supermarketscan.api;
 
 public class Constants {
-//    public final static String BASE_URL = "http://192.168.10.155:5086/api/";
-    public final static String BASE_URL = "http://172.20.10.2:5086/api/";
+    public final static String BASE_URL = "http://192.168.1.166:5086/api/";
 }
-//    public final static String BASE_URL = "https://192.168.10.155:7032/api/";
-//    public final static String BASE_URL = "https://raw.githubusercontent.com/santi-stars/jsonhiberus/";
-
